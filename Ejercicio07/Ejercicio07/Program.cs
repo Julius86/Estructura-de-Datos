f7e5b672c821a1b537ca8b5ac492fc86904280d0
Julius86 <43396786+Julius86@users.noreply.github.com>
2018-09-24 23:00:56 -0700

    Add files via upload

---
+++ Ejercicio07/Ejercicio07/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numAleatorio = new Random();
+            int[] numeros = new int[100];            
+            for (int incremento1=0; incremento1 < numeros.Length; incremento1++)
+            {
+                numeros[incremento1] = numAleatorio.Next(1, 100);
+            }
+            Console.WriteLine("Los números no ordenados son: ");
+            for (int incremento2 = 0; incremento2 < numeros.Length; incremento2++)
+            {
+                Console.WriteLine(numeros[incremento2]);
+            }
+            Array.Sort(numeros);
+            Console.WriteLine("Los números ordenados son: ");
+            for (int incremento3 = 0; incremento3 < numeros.Length; incremento3++)
+            {
+                Console.WriteLine(numeros[incremento3]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio08/Ejercicio08/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            bool condicion = true;            
+            do
+            {
+                int num, rem, sum = 0, temp;
+                Console.Write("Ingrese un número de 5 dígitos: ");
+                num = int.Parse(Console.ReadLine());
+                temp = num;
+                if (num<=99999&&num>=10000)
+                {
+                    while (num > 0)
+                    {
+                        rem = num % 10;
+                        num = num / 10;
+                        sum = sum * 10 + rem;
+                    }
+                    Console.WriteLine("El número escrito inversamente es: {0} \n", sum);
+                    if (temp == sum)
+                    {
+                        Console.WriteLine("El número ingresado es palíndromo.");
+                        condicion = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("El número ingresado no es palíndromo.");
+                        condicion = false;
+                    }                    
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("El número ingresado no es válido. Por favor ingrese un entero de 5 dígitos.");
+                }
+            } while (condicion == true);
+            Console.ReadKey();
+        }
+    }
+}
