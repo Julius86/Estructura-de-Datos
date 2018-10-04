cac98f6494366f81f385f7dc3959c98b7318666e
Julius86 <43396786+Julius86@users.noreply.github.com>
2018-10-03 17:00:04 -0700

    Add files via upload

---
+++ Examen02.-LopezMedranoJulioAntonio/Examen02.-LopezMedranoJulioAntonio/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen02._LopezMedranoJulioAntonio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, temp=0;
+            Console.Write("Ingrese un número de 7 o más dígitos: ");
+            numero = int.Parse(Console.ReadLine());
+            RanaExamen objRanaExamen = new RanaExamen(numero, temp);
+            Console.WriteLine("La suma de cada dígito del número es de: {0}", objRanaExamen.Sumar(numero, temp));
+            Console.ReadKey();
+        }
+    }
+
+    class RanaExamen
+    {
+        int numero;
+        public RanaExamen(int numero, int temp)
+        {
+            this.numero = numero;
+        }
+
+        public int Sumar(int numero, int temp)
+        {
+            if(numero>0)
+            {                
+                return Sumar(numero / 10, temp = numero % 10 + temp); ;
+            }
+            else
+            {
+                return temp;
+            }
+            
+        }
+    }
+}
