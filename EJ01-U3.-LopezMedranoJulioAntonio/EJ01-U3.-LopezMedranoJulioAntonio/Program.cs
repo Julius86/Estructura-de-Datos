67ad2fc355dae75ab057539a36a68c0d9b0af707
Julius86 <43396786+Julius86@users.noreply.github.com>
2018-10-08 23:10:09 -0700

    Add files via upload

---
+++ EJ01-U3.-LopezMedranoJulioAntonio/EJ01-U3.-LopezMedranoJulioAntonio/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace EJ01_U3._LopezMedranoJulioAntonio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numClases=0, cantAlumnos=0;
+            string nombre="", calificacion="";
+            Console.Write("Ingrese la cantidad de clases: ");
+            numClases = int.Parse(Console.ReadLine());
+            Console.Clear();            
+            RanaLista objRana = new RanaLista(numClases, cantAlumnos, nombre, calificacion);
+            objRana.Ranita();
+            Console.ReadKey();
+        }
+    }    
+}
---
+++ EJ01-U3.-LopezMedranoJulioAntonio/EJ01-U3.-LopezMedranoJulioAntonio/RanaLista.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ01_U3._LopezMedranoJulioAntonio
+{
+    class RanaLista
+    {
+        public int numClases, cantAlumnos;
+        public string nombre, calificacion;
+
+        public RanaLista(int numClases, int cantAlumnos, string nombre, string calificacion)
+        {
+            this.numClases = numClases;
+            this.cantAlumnos = cantAlumnos;
+            this.nombre = nombre;
+            this.calificacion = calificacion;
+        }
+
+        public void Ranita()
+        {
+            List<String> NombreClase = new List<String>();
+            for (int i = 0; i < numClases; i++)
+            {
+                Console.Write("Ingrese el nombre de la clase: ");
+                nombre = Console.ReadLine();
+                NombreClase.Add(nombre);
+                Console.Write("Ingrese la cantidad de alumnos en la clase de {0}: ", nombre);
+                cantAlumnos = int.Parse(Console.ReadLine());
+                for (int j = 0; j < cantAlumnos; j++)
+                {
+                    Console.Write("Ingrese la calificación del alumno: ");
+                    calificacion = Console.ReadLine();
+                    NombreClase.Add(calificacion);
+                }
+                Console.Clear();
+            }
+            Console.Clear();
+            foreach (string nombClase in NombreClase)
+            {
+                Console.WriteLine(nombClase);
+            }
+        }
+    }
+}
