32e3b63e0243b01db9210beef6a6af25561de69d
Julius86 <43396786+Julius86@users.noreply.github.com>
2018-10-09 20:26:36 -0700

    Update Program.cs

--- EJ01-U3.-LopezMedranoJulioAntonio/EJ01-U3.-LopezMedranoJulioAntonio/Program.cs
+++ EJ01-U3.-LopezMedranoJulioAntonio/EJ01-U3.-LopezMedranoJulioAntonio/Program.cs
@@ -11,12 +11,16 @@ namespace EJ01_U3._LopezMedranoJulioAntonio
     {
         static void Main(string[] args)
         {
+            //Se declaran las variables            
             int numClases=0, cantAlumnos=0;
             string nombre="", calificacion="";
+            //Se capturan los datos y se limpia con un clear para tener una pantalla más limpia
             Console.Write("Ingrese la cantidad de clases: ");
             numClases = int.Parse(Console.ReadLine());
-            Console.Clear();            
+            Console.Clear();   
+            //Se declara un objeto y se pasan los parámetros
             RanaLista objRana = new RanaLista(numClases, cantAlumnos, nombre, calificacion);
+            //Se manda a llamar el método Ranita
             objRana.Ranita();
             Console.ReadKey();
         }
