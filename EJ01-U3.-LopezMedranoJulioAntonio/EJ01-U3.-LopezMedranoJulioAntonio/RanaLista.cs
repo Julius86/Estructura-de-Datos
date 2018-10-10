3dbefa266e161716792b6fda655a26e070f0b484
Julius86 <43396786+Julius86@users.noreply.github.com>
2018-10-09 20:30:52 -0700

    Update RanaLista.cs

--- EJ01-U3.-LopezMedranoJulioAntonio/EJ01-U3.-LopezMedranoJulioAntonio/RanaLista.cs
+++ EJ01-U3.-LopezMedranoJulioAntonio/EJ01-U3.-LopezMedranoJulioAntonio/RanaLista.cs
@@ -8,9 +8,11 @@ namespace EJ01_U3._LopezMedranoJulioAntonio
 {
     class RanaLista
     {
+        //Se declaran los campos con su respectivo nivel de seguridad
         public int numClases, cantAlumnos;
         public string nombre, calificacion;
-
+        //Se crea el constructor, el cual recibe los parámetros enviados através del objeto 
+        //y se utiliza "this" para mantener las mismas variables
         public RanaLista(int numClases, int cantAlumnos, string nombre, string calificacion)
         {
             this.numClases = numClases;
@@ -18,9 +20,11 @@ namespace EJ01_U3._LopezMedranoJulioAntonio
             this.nombre = nombre;
             this.calificacion = calificacion;
         }
-
+        //Se crea el método Ranita
         public void Ranita()
         {
+            //Se crea una lista, seguido, se crea un for para capturar los nombres de la clase, añadirlos a la lista
+            //y capturar la cantidad de alumnos en cada una de las clases capturadas
             List<String> NombreClase = new List<String>();
             for (int i = 0; i < numClases; i++)
             {
@@ -29,6 +33,7 @@ namespace EJ01_U3._LopezMedranoJulioAntonio
                 NombreClase.Add(nombre);
                 Console.Write("Ingrese la cantidad de alumnos en la clase de {0}: ", nombre);
                 cantAlumnos = int.Parse(Console.ReadLine());
+                //Se crea un for anidado para capturar las calificaciones de los alumnos asignados por el usuario y los agrega a la lista
                 for (int j = 0; j < cantAlumnos; j++)
                 {
                     Console.Write("Ingrese la calificación del alumno: ");
@@ -38,6 +43,7 @@ namespace EJ01_U3._LopezMedranoJulioAntonio
                 Console.Clear();
             }
             Console.Clear();
+            //Se crea un foreach para desplegar cada dato de la lista
             foreach (string nombClase in NombreClase)
             {
                 Console.WriteLine(nombClase);
