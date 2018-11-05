6cdb3d49c60ae5fdbe2b62efe37fdba36a6e29e5
Julius86 <43396786+Julius86@users.noreply.github.com>
2018-11-04 23:54:25 -0800

    Add files via upload

---
+++ PE-3.1-LopezMedranoJulioAnotnio/PE-3.1-LopezMedranoJulioAnotnio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_3._1_LopezMedranoJulioAnotnio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vacas objVacas = new Vacas();
+            Console.WriteLine("Supongamos que Bob tiene cuatro vacas que quiere cruzar por un puente, " +
+                "pero solo un yugo, que puede sostener hasta dos vacas, lado a lado, atadas al yugo. " +
+                "El yugo es demasiado pesado para que lo lleve a través del puente, pero puede atar (y desatar) " +
+                "vacas a él en muy poco tiempo. De sus cuatro vacas, Mazie puede cruzar el puente en 2 minutos, " +
+                "Daisy puede cruzarlo en 4 minutos, Crazy puede cruzarlo en 10 minutos y Lazy puede cruzar en 20 minutos. " +
+                "Por supuesto, cuando dos vacas están atadas al yugo, deben ir a la velocidad de la vaca más lenta. " +
+                "Describe cómo Bob puede conseguir que todas sus vacas crucen el puente en 34 minutos.");
+            objVacas.Inicio();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PE-3.1-LopezMedranoJulioAnotnio/PE-3.1-LopezMedranoJulioAnotnio/Vacas.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_3._1_LopezMedranoJulioAnotnio
+{
+    class Vacas
+    {
+        public int Cruzar { get; set; }
+        public int Mazie = 2, Daisy = 4, Crazy = 10, Lazy = 20;
+        public int tiempo=0, contador=0;
+        List<int> vaquitas = new List<int>();
+        public Vacas()
+        {
+
+        }
+
+        public void Inicio()
+        {              
+            Console.Write("\nElige el número correspondiente a la vaca que desea cruzar:\n1.-Mazie\n2.-Daisy\n3.-Crazy" +
+                "\n4.-Lazy\n5.-Mazie y Daisy" + "\n6.-Crazy y Lazy\n7.-Mazie y Crazy\n8.-Daisy y Lazy\n9.-Mazie y Lazy" +
+                "\n10.-Daisy y Crazy\n");
+            Cruzar = int.Parse(Console.ReadLine());
+            Elegir();
+        }
+
+        public void Elegir()
+        {
+            do
+            {
+                switch (Cruzar)
+                {
+                    case 1:
+                        vaquitas.Add(Mazie);
+                        contador++;
+                        tiempo = vaquitas.Sum();
+                        Console.WriteLine("El tiempo al momento es de: {0}", tiempo);
+                        if (contador < 4)
+                        {
+                            Inicio();
+                        }
+                        else
+                        {
+                            if(tiempo==34)
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, ganase. Felicidades!.", tiempo);
+                            }
+                            else
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, lo siento, perdiste.", tiempo);
+                            }                            
+                        }
+                        break;
+
+                    case 2:
+                        vaquitas.Add(Daisy);
+                        contador++;
+                        tiempo = vaquitas.Sum();
+                        Console.WriteLine("El tiempo al momento es de: {0}", tiempo);
+                        if (contador < 4)
+                        {                            
+                            Inicio();
+                        }
+                        else
+                        {
+                            if (tiempo == 34)
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, ganase. Felicidades!.", tiempo);
+                            }
+                            else
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, lo siento, perdiste.", tiempo);
+                            }
+                        }
+                        break;
+
+                    case 3:
+                        vaquitas.Add(Crazy);
+                        contador++;
+                        tiempo = vaquitas.Sum();
+                        Console.WriteLine("El tiempo al momento es de: {0}", tiempo);
+                        if (contador < 4)
+                        {
+                            Inicio();
+                        }
+                        else
+                        {
+                            if (tiempo == 34)
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, ganase. Felicidades!.", tiempo);
+                            }
+                            else
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, lo siento, perdiste.", tiempo);
+                            }
+                        }
+                        break;
+
+                    case 4:
+                        vaquitas.Add(Lazy);
+                        contador++;
+                        tiempo = vaquitas.Sum();
+                        Console.WriteLine("El tiempo al momento es de: {0}", tiempo);
+                        if (contador < 4)
+                        {
+                            Inicio();
+                        }
+                        else
+                        {
+                            if (tiempo == 34)
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, ganase. Felicidades!.", tiempo);
+                            }
+                            else
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, lo siento, perdiste.", tiempo);
+                            }
+                        }
+                        break;
+
+                    case 5:
+                        Mazie = 0;
+                        vaquitas.Add(Mazie);
+                        vaquitas.Add(Daisy);
+                        contador = contador + 2;
+                        tiempo = vaquitas.Sum();
+                        Console.WriteLine("El tiempo al momento es de: {0}", tiempo);
+                        if (contador < 4)
+                        {                            
+                            Inicio();
+                        }
+                        else
+                        {
+                            if (tiempo == 34)
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, ganase. Felicidades!.", tiempo);
+                            }
+                            else
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, lo siento, perdiste.", tiempo);
+                            }
+                        }
+                        break;
+
+                    case 6:
+                        Crazy = 0;
+                        vaquitas.Add(Crazy);
+                        vaquitas.Add(Lazy);
+                        contador = contador + 2;
+                        tiempo = vaquitas.Sum();
+                        Console.WriteLine("El tiempo al momento es de: {0}", tiempo);
+                        if (contador < 4)
+                        {
+                            Inicio();
+                        }
+                        else
+                        {
+                            if (tiempo == 34)
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, ganase. Felicidades!.", tiempo);
+                            }
+                            else
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, lo siento, perdiste.", tiempo);
+                            }
+                        }
+                        break;
+
+                    case 7:
+                        Mazie = 0;
+                        vaquitas.Add(Mazie);
+                        vaquitas.Add(Crazy);
+                        contador = contador + 2;
+                        tiempo = vaquitas.Sum();
+                        Console.WriteLine("El tiempo al momento es de: {0}", tiempo);
+                        if (contador < 4)
+                        {
+                            Inicio();
+                        }
+                        else
+                        {
+                            if (tiempo == 34)
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, ganase. Felicidades!.", tiempo);
+                            }
+                            else
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, lo siento, perdiste.", tiempo);
+                            }
+                        }
+                        break;
+
+                    case 8:
+                        Daisy = 0;
+                        vaquitas.Add(Daisy);
+                        vaquitas.Add(Lazy);
+                        contador = contador + 2;
+                        tiempo = vaquitas.Sum();
+                        Console.WriteLine("El tiempo al momento es de: {0}", tiempo);
+                        if (contador < 4)
+                        {
+                            Inicio();
+                        }
+                        else
+                        {
+                            if (tiempo == 34)
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, ganase. Felicidades!.", tiempo);
+                            }
+                            else
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, lo siento, perdiste.", tiempo);
+                            }
+                        }
+                        break;
+
+                    case 9:
+                        Mazie = 0;
+                        vaquitas.Add(Mazie);
+                        vaquitas.Add(Lazy);
+                        contador = contador + 2;
+                        tiempo = vaquitas.Sum();
+                        Console.WriteLine("El tiempo al momento es de: {0}", tiempo);
+                        if (contador < 4)
+                        {
+                            Inicio();
+                        }
+                        else
+                        {
+                            if (tiempo == 34)
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, ganase. Felicidades!.", tiempo);
+                            }
+                            else
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, lo siento, perdiste.", tiempo);
+                            }
+                        }
+                        break;
+
+                    case 10:
+                        Daisy = 0;
+                        vaquitas.Add(Daisy);
+                        vaquitas.Add(Crazy);
+                        contador = contador + 2;
+                        tiempo = vaquitas.Sum();
+                        Console.WriteLine("El tiempo al momento es de: {0}", tiempo);
+                        if (contador < 4)
+                        {
+                            Inicio();
+                        }
+                        else
+                        {                            
+                            if (tiempo == 34)
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, ganase. Felicidades!.", tiempo);
+                            }
+                            else
+                            {
+                                Console.WriteLine("El tiempo es de : {0}, lo siento, perdiste.", tiempo);
+                            }
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            } while (contador < 4);
+        }
+    }
+}
